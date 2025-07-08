8322318c639c3f9fea1e74795ff6ee9cd5897336
rasel-093 <rasel.cse19.ruet@gmail.com>
2025-07-09 00:41:51 +0600

    Cupertino completed

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ import 'package:flutter_basic/statefulwidget/StatefulWidget.dart';
 import 'package:flutter_basic/stateless_widget/StatelessWidget.dart';
 import 'package:flutter_basic/styled_widget/StyledWidget.dart';
 import 'package:flutter_basic/material_widget/MaterialWidget.dart';
+import 'package:flutter_basic/widget/cupertino_widget/CupertinoWidget.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   //runApp(const MyResponsiveApp());
   //runApp(const StatelesswidgetApp());
   //runApp(const StatefulWidgetApp());
   //runApp(const StyledWidgetApp());
-  runApp(const MaterialShowcaseApp());
+  //runApp(const MaterialShowcaseApp());
+  runApp(const CupertinoAppDemo());
 }
---
+++ lib/widget/cupertino_widget/CupertinoWidget.dart
@@ -0,0 +1,158 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+
+// https://docs.flutter.dev/ui/widgets/cupertino
+class CupertinoAppDemo extends StatelessWidget {
+  const CupertinoAppDemo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      title: "Cupertino Widget Demo",
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+  @override
+  State<HomeScreen> createState() => _HomeScreen();
+}
+
+class _HomeScreen extends State<HomeScreen> {
+  bool isSwitched = false;
+  String selectedMonth = "January"; // Default value for selected month
+  @override
+  Widget build(BuildContext context) {
+    return cupertinoWidgetScreen(context);
+  }
+
+  Widget cupertinoWidgetScreen(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          cupertinoButton(context, "Alert Dialog", () {
+            showCupertinoDialog(
+              context: context,
+              builder:
+                  (context) => CupertinoAlertDialog(
+                    title: const Text("Alert Dialog"),
+                    content: const Text("This is a Cupertino AlertDialog."),
+                    actions: [
+                      CupertinoDialogAction(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text("Close"),
+                      ),
+                      CupertinoDialogAction(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: const Text("Confirm"),
+                      ),
+                    ],
+                  ),
+            );
+          }),
+          const SizedBox(height: 20),
+          cupertinoButton(context, "Action Sheet", () {
+            showActionSheet(context, (month) {
+              setState(() {
+                selectedMonth = month;
+              });
+            });
+          }),
+          const SizedBox(height: 20),
+          Text(
+            "Selected Month: $selectedMonth",
+            style: const TextStyle(fontSize: 20),
+          ),
+          Text(
+            "Switch is ${isSwitched ? 'ON' : 'OFF'}",
+            style: const TextStyle(fontSize: 20),
+          ),
+          CupertinoSwitch(
+            value: isSwitched,
+            onChanged: (value) {
+              setState(() {
+                isSwitched = value;
+              });
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+cupertinoButton(BuildContext context, String btnText, Function onClick) {
+  return CupertinoButton(
+    color: CupertinoColors.activeBlue,
+    child: Text(btnText, style: TextStyle(color: CupertinoColors.white)),
+    onPressed: () {
+      onClick();
+    },
+  );
+}
+
+showActionSheet(BuildContext context, Function(String) onSelect) {
+  showCupertinoModalPopup<void>(
+    context: context,
+    builder:
+        (BuildContext context) => CupertinoActionSheet(
+          title: const Text('Select Month'),
+          message: const Text('Select a month from the list below.'),
+          actions: <CupertinoActionSheetAction>[
+            CupertinoActionSheetAction(
+              isDefaultAction: true,
+              onPressed: () {
+                onSelect('January');
+                Navigator.pop(context);
+              },
+              child: const Text('January'),
+            ),
+            CupertinoActionSheetAction(
+              onPressed: () {
+                onSelect('February');
+                Navigator.pop(context);
+              },
+              child: const Text('February'),
+            ),
+            CupertinoActionSheetAction(
+              onPressed: () {
+                onSelect('March');
+                Navigator.pop(context);
+              },
+              child: const Text('March'),
+            ),
+            CupertinoActionSheetAction(
+              onPressed: () {
+                onSelect('April');
+                Navigator.pop(context);
+              },
+              child: const Text('April'),
+            ),
+            CupertinoActionSheetAction(
+              onPressed: () {
+                onSelect('May');
+                Navigator.pop(context);
+              },
+              child: const Text('May'),
+            ),
+            CupertinoActionSheetAction(
+              onPressed: () {
+                onSelect('June');
+                Navigator.pop(context);
+              },
+              child: const Text('June'),
+            ),
+          ],
+        ),
+  );
+}
